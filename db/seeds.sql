1d47f27dfaea2009af83329b9a935db91c60df86
Anand Gopalakrishnan <anand@Anands-MBP.home>
2016-08-05 19:29:15 -0400

    Created package.json, empty server.js. Created db folder. Added schema.sql to create database burgers_db and table burgers. Added seeds.sql with three rows for burgers.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured, date) VALUES ('Bacon Burger', FALSE, now());
+INSERT INTO burgers_db.burgers (burger_name, devoured, date) VALUES ('Teriyaki Burger', FALSE, now());
+INSERT INTO burgers_db.burgers (burger_name, devoured, date) VALUES ('Cheese Burger', FALSE, now());
